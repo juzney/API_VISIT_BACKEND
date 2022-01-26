e11f9dd35b52dd1004fbf660dfa9fd901a5fa1e9
juzney <juzneychicopera@gmail.com>
2022-01-26 14:24:41 +0200

    adding remote database

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/tongaat',
+mongoose.connect('mongodb+srv://webjuzney:L4ypVafHJ715OS5J@cluster0.xkoov.mongodb.net/appVisit?retryWrites=true&w=majority',
 { useUnifiedTopology: true,
  useNewUrlParser: true,
  useCreateIndex: true,  }
